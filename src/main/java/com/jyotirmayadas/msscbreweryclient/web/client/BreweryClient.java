c8ed00c19e9061dc4b3268b2c61cbda3a75290e2
Jitu7 <jyotirmaya12@gmail.com>
2020-04-23 15:46:15 +0530

    http-get

---
+++ src/main/java/com/jyotirmayadas/msscbreweryclient/web/client/BreweryClient.java
@@ -0,0 +1,36 @@
+package com.jyotirmayadas.msscbreweryclient.web.client;
+
+import com.jyotirmayadas.msscbreweryclient.web.model.BeerDto;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+@ConfigurationProperties(prefix = "brewery")
+@Component
+public class BreweryClient {
+
+    public final String BEER_PATH_V1 = "/api/v1/beer/";
+    private String apiHost;
+
+    private final RestTemplate restTemplate;
+
+    public BreweryClient(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public BeerDto getBeerById(UUID uuid) {
+        ResponseEntity<BeerDto> beerDtoResponseEntity =
+                restTemplate.getForEntity(apiHost + BEER_PATH_V1 + uuid.toString(), BeerDto.class);
+        BeerDto beerDto = beerDtoResponseEntity.getBody();
+
+        return beerDto;
+    }
+
+    public void setApiHost(String apiHost) {
+        this.apiHost = apiHost;
+    }
+}
---
+++ src/test/java/com/jyotirmayadas/msscbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,24 @@
+package com.jyotirmayadas.msscbreweryclient.web.client;
+
+import com.jyotirmayadas.msscbreweryclient.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient breweryClient;
+
+    @Test
+    void getBeerById() {
+        BeerDto beerById = breweryClient.getBeerById(UUID.randomUUID());
+
+        assertNotNull(beerById);
+    }
+}
